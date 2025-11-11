bbd32e39b1f8c9d3510a736932fa2705dce3aee5
NoorhanR <rashidnoorhan@gmail.com>
2025-11-10 22:45:55 -0600

    11/10/2025 10:42PM I have made a bank.java. This program manages the setup and the initializations of the semaphores. My thoughts: This sets up the semaphores for bankDoor, manager, safe, and availableTellers. I have only set this up to handle 3 customers, and after working on the other programs, and testing, I will make it handle more customers. Plan: Work on the teller.java, and the customer.java programs.

---
+++ src/bank.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+
+public class bank
+{
+    public static final int NUM_TELLERS = 3;
+
+    // Only two people, at a time, can go through the door
+    public static Semaphore bankDoor = new Semaphore(2, true);
+    // 1 person talks to manager at a time
+    public static Semaphore manager = new Semaphore(1, true);
+    // 2 tellers at the safe
+    public static Semaphore safe = new Semaphore(2, true);
+    // Available tellers
+    public static Semaphore availableTellers = new Semaphore(NUM_TELLERS, true);
+
+    public static Account sharedAccount = new Account(100);
+    public static Teller[] teller = new Teller[NUM_TELLERS];
+
+    public void openBank()
+    {
+        System.out.println("Bank is opening");
+
+        // Start teller threads
+        for (int i = 0; i < NUM_TELLERS; i++)
+        {
+            teller[i] = new Teller(i);
+            teller[i].start();
+        }
+
+        // Start customer threads
+        for (int i = 0; i < NUM_TELLERS; i++)
+        {
+            new Customer(i).start();
+        }
+    }
+}
